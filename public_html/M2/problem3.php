7057c4b8de407ebba564ae9f1dbe63efa2d7a29c
jayp0671 <jayveera0315@gmail.com>
2023-09-30 17:38:37 -0400

    fixes

--- public_html/M2/problem3.php
+++ public_html/M2/problem3.php
@@ -1,26 +1,18 @@
 <?php
-$a1 = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10];
-$a2 = [-1, 1, -2, 2, 3, -3, -4, 5];
-$a3 = [-0.01, -0.0001, -.15];
-$a4 = ["-1", "2", "-3", "4", "-5", "5", "-6", "6", "-7", "7"];
-
 function bePositive($arr) {
     echo "Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "Positive output:<br>";
-
+    
     foreach ($arr as $value) {
-        if ($value < 0 || $value === "-0") {
-            echo abs($value) . "<br>";
-        } else {
-            echo $value . "<br>";
-        }
+        $positiveValue = $value >= 0 ? $value : abs($value);
+        echo var_export($positiveValue, true) . "<br>";
     }
 }
 
 echo "Problem 3: Be Positive<br>";
 ?>
 <table>
-    <thead> <!-- Fixed typo here -->
+    <thead>
         <th>A1</th>
         <th>A2</th>
         <th>A3</th>
